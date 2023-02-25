8c9730ffb5c69907b8413b04d827c225d847f6e4
reyu <ka15sugar@gmail.com>
2023-02-25 21:36:47 +0900

    fix qr

--- go/main.go
+++ go/main.go
@@ -215,16 +215,9 @@ func generateQRCode(id string) ([]byte, error) {
 		return nil, err
 	}
 
-	/*
-		生成するQRコードの仕様
-		 - PNGフォーマット
-		 - QRコードの1モジュールは1ピクセルで表現
-		 - バージョン5 (37x37ピクセル、マージン含め45x45ピクセル)
-		 - エラー訂正レベルM (15%)
-	*/
-	_, err = exec.
-		Command("qrencode", "-o", qrCodeFileName, "-t", "PNG", "-s", "1", "-v", "5", "--strict-version", "-l", "M", encryptedID).
-		Output()
+	err = exec.
+		Command("sh", "-c", fmt.Sprintf("echo \"%s\" | qrencode -o %s -t PNG -s 1 -v 6 --strict-version -l M", encryptedID, qrCodeFileName)).
+		Run()
 	if err != nil {
 		return nil, err
 	}
