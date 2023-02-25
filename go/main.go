253b74b1163a361aa04e2ef4807f23e910f268d4
reyu <ka15sugar@gmail.com>
2023-02-25 22:12:11 +0900

    fix

--- go/main.go
+++ go/main.go
@@ -35,8 +35,6 @@ func main() {
 		log.Println(http.ListenAndServe("localhost:6060", nil))
 	}()
 
-	os.Setenv("TZ", "UTC")
-
 	host := getEnvOrDefault("DB_HOST", "localhost")
 	port := getEnvOrDefault("DB_PORT", "3306")
 	user := getEnvOrDefault("DB_USER", "isucon")
